0900d41572bb4aa2ca6061b90820f36d6ece3b63
Matthew Parry-Wilkes <tonicblue@gmail.com>
2020-02-10 14:50:30 +0000

    More import ajax testing

--- includes/posts-importer.php
+++ includes/posts-importer.php
@@ -13,7 +13,7 @@ function tfn_import_posts() {
     $results = array();
 
     foreach( $posts as $post ) {
-      $result = import_tfn_post( $post );
+      $result = tfn_import_post( $post );
       array_push($results, $result);
     }
 
@@ -29,7 +29,7 @@ add_action( 'wp_ajax_nopriv_import-tfn-posts', 'tfn_import_posts' );
 
 function tfn_import_post( $post ) {
   try {
-    //tfn_is_post_valid( $post );
+    tfn_is_post_valid( $post );
 
     return $post['title'];
   } catch (Exception $ex) {
@@ -37,15 +37,15 @@ function tfn_import_post( $post ) {
   }
 }
 
-// function tfn_is_post_valid( $post ) {
-//   if (!is_string($post["url"])) { throw new Exception("Post url invalid"); }
-//   if (!is_string($post["title"])) { throw new Exception("Post title invalid"); }
-//   if (!is_string($post["copy"])) { throw new Exception("Post copy invalid"); }
-//   if (!is_string($post["postType"])) { throw new Exception("Post postType invalid"); }
-//   if (!is_string($post["category"])) { throw new Exception("Post category invalid"); }
-//   if (!is_string($post["slug"])) { throw new Exception("Post slug invalid"); }
-//   if (!is_string($post["author"])) { throw new Exception("Post author invalid"); }
-//   if (!is_string($post["date"])) { throw new Exception("Post date invalid"); }
-//   if (!is_string($post["ogDescription"])) { throw new Exception("Post ogDescription invalid"); }
-//   if (!is_array($post["blocks"])) { throw new Exception("Post blocks invalid"); }
-// }
+function tfn_is_post_valid( $post ) {
+  if (!is_string($post["url"])) { throw new Exception("Post url invalid"); }
+  if (!is_string($post["title"])) { throw new Exception("Post title invalid"); }
+  if (!is_string($post["copy"])) { throw new Exception("Post copy invalid"); }
+  if (!is_string($post["postType"])) { throw new Exception("Post postType invalid"); }
+  if (!is_string($post["category"])) { throw new Exception("Post category invalid"); }
+  if (!is_string($post["slug"])) { throw new Exception("Post slug invalid"); }
+  if (!is_string($post["author"])) { throw new Exception("Post author invalid"); }
+  if (!is_string($post["date"])) { throw new Exception("Post date invalid"); }
+  if (!is_string($post["ogDescription"])) { throw new Exception("Post ogDescription invalid"); }
+  if (!is_array($post["blocks"])) { throw new Exception("Post blocks invalid"); }
+}
