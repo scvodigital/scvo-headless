2c3bc3f227b4faa6716e28b09d8666c42252b269
Matthew Parry-Wilkes <tonicblue@gmail.com>
2020-02-10 11:38:13 +0000

    test

--- blocks/index.php
+++ blocks/index.php
@@ -45,34 +45,68 @@ function display_blocks_meta_box( $post ) {
   $prettified = json_encode($unslashed, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
   $escaped = htmlentities($prettified);
 
-  echo 'What did I break?';
+  echo <<<EOD
+  <div style="text-align: right;">
+    <button type="button" id="post-blocks-json-copy" class="components-button is-button is-primary">
+      Copy
+    </button>
+  </div>
+  <textarea id="post-blocks-json" style="height: 50vh; width: 100%; font-family: monospace; border: 1px solid #dadada; border-radius: 0;">${escaped}</textarea>
+  <script>
+    document.addEventListener('DOMContentLoaded', () => {
+      const copyButton = document.querySelector('#post-blocks-json-copy');
+      const textbox = document.querySelector('#post-blocks-json');
+
+      copyButton.addEventListener('click', () => {
+        textbox.select();
+        document.execCommand('copy');
+      });
+
+      const debounce = null;
+      window.wp.data.subscribe(() => {
+        if (debounce) {
+          window.clearTimeout(debounce);
+        }
+
+        debounce = window.setTimeout(() => {
+          const url = '/wp-admin/admin-ajax.php?action=get-post-gutenberg-json&id=${id}} ?>';
+          console.log('Getting Gutenberg JSON from', url);
+          jQuery.getJSON(url, {}, (data, status, xhr) => {
+            console.log(data, status, xhr);
+            jQuery(textbox).html(data);
+          });
+        }, 1000);
+      });
+    });
+  </script>
+  EOD;
 }
 
 /**
  * Ajax action to get a post's Gutenberg JSON
  */
 
-function get_post_gutenberg_json() {
-  header( 'content-type: application/json' );
+// function get_post_gutenberg_json() {
+//   header( 'content-type: application/json' );
 
-  try {
-    if (empty($_GET['id'])) {
-      throw new Exception( 'No content Id provided' );
-    }
+//   try {
+//     if (empty($_GET['id'])) {
+//       throw new Exception( 'No content Id provided' );
+//     }
 
-    $json = get_post_meta( $_GET['id'], 'blocks', true );
+//     $json = get_post_meta( $_GET['id'], 'blocks', true );
 
-    $decoded = json_decode($json);
-    $unslashed = wp_unslash($decoded);
-    $prettified = json_encode($unslashed, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+//     $decoded = json_decode($json);
+//     $unslashed = wp_unslash($decoded);
+//     $prettified = json_encode($unslashed, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
-    echo $prettified;
-  } catch( Exception $ex) {
-    echo json_encode($ex);
-  }
+//     echo $prettified;
+//   } catch( Exception $ex) {
+//     echo json_encode($ex);
+//   }
 
-  wp_die();
-}
+//   wp_die();
+// }
 
-add_action( 'wp_ajax_get-post-gutenberg-json', 'get_post_gutenberg_json' );
+// add_action( 'wp_ajax_get-post-gutenberg-json', 'get_post_gutenberg_json' );
 ?>
