c7f7825de5bfab596fa3711d820f0af8eeed5492
Matthew Parry-Wilkes <tonicblue@gmail.com>
2020-02-10 10:59:20 +0000

    Ajax action to get post's blocks JSON

--- blocks/index.php
+++ blocks/index.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Action to automatically get Gutenberg JSON on save of any Gutenberg content
+ */
+
 function status_transition_update_blocks_json( $new_status, $old_status, $post ) {
   $validStatuses = [ "publish", "pending", "future", "private", "trash" ];
   if ( in_array( $new_status, $validStatuses ) && isset( $post->post_content ) ) {
@@ -10,6 +14,10 @@ function status_transition_update_blocks_json( $new_status, $old_status, $post )
   }
 }
 add_action( 'transition_post_status',  'status_transition_update_blocks_json', 10, 3 );
+
+/**
+ * Add a meta box to pages that have Gutenberg content to display their JSON
+ */
 
 function register_blocks_meta_box() {
   $args = array(
@@ -41,3 +49,32 @@ function display_blocks_meta_box( $post ) {
   echo "</div>";
   echo "<textarea id='post-blocks-json' style='height: 50vh; width: 100%; font-family: monospace; border: 1px solid #dadada; border-radius: 0;'>$escaped</textarea>";
 }
+
+/**
+ * Ajax action to get a post's Gutenberg JSON
+ */
+
+function get_post_gutenberg_json() {
+  header( 'content-type: application/json' );
+
+  try {
+    if (empty($_GET['id'])) {
+      throw new Exception( 'No content Id provided' );
+    }
+
+    $json = get_post_meta( $_GET['id'], 'blocks', true );
+
+    $decoded = json_decode($json);
+    $unslashed = wp_unslash($decoded);
+    $prettified = json_encode($unslashed, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    $escaped = htmlentities($prettified);
+
+    echo $escaped;
+  } catch( Exception $ex) {
+    echo json_encode($ex);
+  }
+
+  wp_die();
+}
+
+add_action( 'wp_ajax_get-post-gutenberg-json', 'get_post_gutenberg_json' );
