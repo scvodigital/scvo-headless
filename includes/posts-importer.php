67b7f4a27023e8701c241f0f6bcf5de902d2c692
Matthew Parry-Wilkes <tonicblue@gmail.com>
2020-02-10 13:53:53 +0000

    More import ajax testing

--- includes/posts-importer.php
+++ includes/posts-importer.php
@@ -1,12 +1,12 @@
 <?php
 
 function import_tfn_posts() {
-  //header( 'content-type: application/json' );
+  header( 'content-type: application/json' );
 
   try {
-    $data = json_decode(file_get_contents('php://input'), true);
+    $posts = json_decode(file_get_contents('php://input'), true);
 
-    print_r($data);
+    echo json_encode($data);
   } catch( Exception $ex ) {
     echo '{ "error": ' . json_encode($ex->getMessage()) . '" }';
   }
