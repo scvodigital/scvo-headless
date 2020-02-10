472452ff9344768e1f6c35bf0355996c5ec476b5
Matthew Parry-Wilkes <tonicblue@gmail.com>
2020-02-10 13:57:35 +0000

    More import ajax testing

--- includes/posts-importer.php
+++ includes/posts-importer.php
@@ -6,7 +6,18 @@ function import_tfn_posts() {
   try {
     $posts = json_decode(file_get_contents('php://input'), true);
 
-    echo json_encode($data);
+    if (!is_array($posts)) {
+      throw new Exception( 'Was expecting an array of posts' );
+    }
+
+    $results = array();
+
+    foreach( $posts as $post ) {
+      $result = import_tfn_post( $post );
+      array_push($results, $result);
+    }
+
+    echo json_encode($results);
   } catch( Exception $ex ) {
     echo '{ "error": ' . json_encode($ex->getMessage()) . '" }';
   }
@@ -15,3 +26,7 @@ function import_tfn_posts() {
 }
 
 add_action( 'wp_ajax_nopriv_import-tfn-posts', 'import_tfn_posts' );
+
+function import_tfn_post( $post ) {
+  return $post->title;
+}
