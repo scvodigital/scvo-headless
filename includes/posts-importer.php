eb5a36a4826bf2ae5e34871902df9045eb64e23b
Matthew Parry-Wilkes <tonicblue@gmail.com>
2020-02-10 13:19:09 +0000

    Adding importer page for testing a TFN import endpoint

--- includes/posts-importer.php
+++ includes/posts-importer.php
@@ -1,6 +1,10 @@
 <?php
 
-add_menu_page( 'TFN Importer', 'TFN Importer', 'manage_options', 'tfn-importer', 'tfn_importer_page' );
+function register_tfn_importer_page() {
+  add_menu_page( 'TFN Importer', 'TFN Importer', 'manage_options', 'tfn-importer', 'tfn_importer_page' );
+}
+
+add_action( 'admin_init', 'register_tfn_importer_page' );
 
 function tfn_importer_page() {
   echo 'Dope';
